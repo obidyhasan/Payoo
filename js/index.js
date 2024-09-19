2155607e092c33d9f2bc81c421afbbd74559a68d
obidyhasan <obidyhasan@gmail.com>
2024-09-19 17:38:15 +0600

    inlcude index.html

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-console.log("Connected.....");
+const number = document.getElementById("number");
+const pin = document.getElementById("pin");
+
+document.getElementById("loginBtn").addEventListener("click", function (event) {
+  event.preventDefault();
+  if (number.value === "01625319213" && pin.value === "1234") {
+    window.location.href = "home.html";
+  } else {
+    alert("Invalid Number or Pin!!!");
+  }
+});
